547106589b00f9566eaa684d2e27fc9ec547e7b0
Abhay <97611306+Abhay557@users.noreply.github.com>
2022-09-13 15:25:09 +0530

    Added Search Endpoint Wrapper

--- public/index.js
+++ public/index.js
@@ -1,12 +1,28 @@
-const express = require('express');
-const { search } = require('./functions/search');
-const { pchecker } = require('./helpers/checkparams');
-
-var app = module.exports = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.get('/search', pchecker ,search);
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const axios = require('axios');
+module.exports = async (search, context) => {
+  console.log(search);
+  let url = `https://www.zoroapi.tk/search?name=${search}`;
+  let status = `success`;
+  let amount = `1`;
+  console.log(url);
+  let response = await axios.get(url);
+  console.log(response.data);
+  let query =
+    response.data?.results.length > 0 ? response.data.results[0] : null;
+  if (query == null) {
+    throw new Error(`Invalid: "${search}" No Anime Found!`);
+    return (response) => response.data();
+  }
+  return {
+    status: `${status}`,
+    search: `${search}`,
+    amount: `${amount}`,
+    anime: {
+      name: `${query.title}`,
+      episodes: `${query.episodes}`,
+      duration: `${query.duration}`,
+      languages: `${query.languages}`,
+      link: `${query.link}`,
+    },
+  };
+}
